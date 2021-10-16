569b261d050f4cce45b24224757459a6e97e6b64
seattleriot <seattleriot@hotmail.com>
2021-10-16 15:31:02 -0700

    displaying searched cities from local storage as links

--- scripts.js
+++ scripts.js
@@ -1,31 +1,18 @@
 $(document).ready(function () {
   let city;
-  let longitude;
-  let latitude;
+
   const APIKey = "44c329c1fb39cc90b982fb588f6a68c5";
   let invalidSearchEl = $("#invalidSearch");
   let currentEl = $("#current");
   let forecastEl = $("#forecast");
 
   let searchedCities = getLocalStorage();
+
   if (!searchedCities) {
     searchedCities = [];
+  } else {
+    printSearchedNav();
   }
-
-  let humidity;
-  let windSpeed;
-  let weatherDescription;
-  let weatherMain;
-  let weatherImg;
-
-  let weatherIconEl = $("#weatherIcon");
-  let cityEl = $("#city");
-  let fahrenheitEl = $("#fahrenheit");
-  let windSpeedEl = $("#windSpeed");
-  let humidityEl = $("#humidity");
-  let weatherMainEl = $("#weatherMain");
-  let weatherDescriptionEl = $("#weatherDescription");
-  let cityNavEl = $("nav ul#searchedCities");
 
   //   setInterval(function () {
   //     city ?? fetchData();
@@ -41,14 +28,28 @@ $(document).ready(function () {
     return localStorageCities;
   }
 
-  function setLocalStorage() {
-    searchedCities.find((c) => c === city) ?? searchedCities.push(city);
-    localStorage.setItem("searchedCities", JSON.stringify(searchedCities));
-
-    for (let i = 0; i < searchedCities.length; i++) {
-      cityNavEl.append(searchedCities[i]);
+  function setLocalStorage(data) {
+    city = data.name;
+    if (!searchedCities.find((c) => c === city)) {
+      searchedCities.push(city);
     }
+    localStorage.setItem("searchedCities", JSON.stringify(searchedCities));
   }
+
+  function printSearchedNav() {
+    let searchedCitiesEl = $("#searchedCities");
+    searchedCitiesEl.html("");
+    searchedCities.forEach((city) => {
+      let a = $("<a href='#' class='city'>");
+      a.text(city);
+      searchedCitiesEl.append(a);
+    });
+  }
+
+  $(document).on("click", ".city", function (e) {
+    city = e.currentTarget.innerText;
+    getLongLat(city);
+  });
 
   function getLongLat(city) {
     resetPage();
@@ -60,14 +61,12 @@ $(document).ready(function () {
             `Bad Request. Request responded with: ${response.status}`
           );
         }
-        setLocalStorage();
+
         return response.json();
       })
       .then(function (data) {
-        console.log(data.coord.lat);
-        latitude = data.coord.lat;
-        longitude = data.coord.lon;
-        fetchOneCallAPI(latitude, longitude);
+        setLocalStorage(data);
+        fetchOneCallAPI(data.coord.lat, data.coord.lon);
       })
       .catch(function (err) {
         handleError(err);
@@ -78,39 +77,58 @@ $(document).ready(function () {
     const URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${APIKey}&units=imperial`;
     fetch(URL)
       .then(function (response) {
-        if (response.status === 404) {
-          handle404();
-          return;
+        if (response.status !== 200) {
+          throw new Error(
+            `Bad Request. Request responded with: ${response.status}`
+          );
         }
         return response.json();
       })
       .then(function (data) {
-        console.log("fetchOneCallAPI", data);
-        // TODO create function to populate DOM
-        setCurrentWeather(data.current);
-        setForecastWeather(data.daily);
+        setCurrentWeather(data);
+        setForecastWeather(data);
+      })
+      .catch(function (err) {
+        handleError(err);
       });
+    printSearchedNav();
   }
 
-  function setCurrentWeather(current) {
+  function setCurrentWeather(data) {
+    let current = data.current;
+
+    // create HTML elements
+    let spanCityEl = $("<span class='city'>");
     let spanSunriseEl = $("<span>");
     let spanSunsetEl = $("<span>");
     let spanTimeEl = $("<span>");
     let spanHumidityEl = $("<span>");
     let spanWindSpeedEl = $("<span>");
-    let spanTempEl = $("<span>");
-    let spanIconEl = $("<span>");
-    let spanUviEl = $("<span>");
+    let spanTempEl = $("<span class='temp'>");
+    let spanDescriptionEl = $("<span class='description'>");
+    let spanHiLoEl = $("<span class='hiLo'>");
+    let spanIconEl = $("<span class='icon'>");
+    let spanUviEl = $("<span class='uvi'>");
     let spanWindEl = $("<span>");
+
+    let leftRightContainer = $('<span id="display">');
+    let leftEl = $('<span class="left">');
+    let rightEl = $('<span class="right">');
+
+    let spanTempDescriptionEl = $("<span class='tempDescription'>");
 
     let sunrise = moment.unix(current.sunrise).format("h:mm a");
     let sunset = moment.unix(current.sunset).format("h:mm a");
     let time = moment.unix(current.dt).format("MMMM Do YYYY, h:mm:ss a");
-    let humidity = `${current.humidity}%`;
+    let humidity = getHumidity(current.humidity);
     let temp = `${current.temp.toFixed(0)}&#176;`;
-    let windSpeed = `${current.wind_speed} MPH`;
+    let description = current.weather[0].main;
+    let hiLo = `${data.daily[0].temp.max.toFixed(
+      0
+    )}&#176;/${data.daily[0].temp.min.toFixed(0)}&#176;`;
+    let windSpeed = getWindSpeed(current.wind_speed);
     let icon = getWeatherIcon(current.weather[0].icon);
-    let uvi = current.uvi;
+    let uvi = getUviHtml(current.uvi);
     let wind = {
       deg: current.wind_deg,
       speed: current.wind_speed,
@@ -123,22 +141,37 @@ $(document).ready(function () {
       main: w.main,
     };
 
+    // add content to html elements
+    spanCityEl.text(city);
     spanTimeEl.text(time);
     spanTempEl.html(temp);
+    spanDescriptionEl.html(description);
     spanIconEl.html(icon);
+    spanHiLoEl.html(hiLo);
     spanHumidityEl.text(humidity);
     spanWindSpeedEl.text(windSpeed);
+    spanUviEl.html(uvi);
 
-    currentEl
-      .append(spanTimeEl)
+    leftEl
       .append(spanTempEl)
+      .append(spanHiLoEl)
       .append(spanIconEl)
-      .append(spanHumidityEl)
-      .append(spanWindSpeedEl);
+      .append(spanDescriptionEl);
+    rightEl.append(spanHumidityEl).append(spanWindSpeedEl).append(spanUviEl);
+    leftRightContainer.append(leftEl).append(rightEl);
+
+    currentEl.append(spanCityEl).append(spanTimeEl).append(leftRightContainer);
+
+    currentEl.css("display", "flex");
   }
 
-  function setForecastWeather(daily) {
-    daily = daily.slice(1, 6); // just need 5 day forecast
+  function setForecastWeather(data) {
+    let currentDay = moment.unix(data.current.dt).format("MMMM Do YYYY");
+    let firstForecastDay = moment.unix(data.daily[0].dt).format("MMMM Do YYYY");
+    daily =
+      currentDay !== firstForecastDay
+        ? data.daily.slice(2, 7)
+        : data.daily.slice(1, 6);
 
     daily.forEach((day) => {
       let divEl = $("<div>");
@@ -153,8 +186,8 @@ $(document).ready(function () {
       let hi = `${day.temp.max.toFixed(0)}&#176;`;
       let lo = `${day.temp.min.toFixed(0)}&#176;`;
       let icon = getWeatherIcon(day.weather[0].icon);
-      let humidity = `Humidity ${day.humidity} %`;
-      let windSpeed = `Wind: ${day.wind_speed} MPH`;
+      let humidity = getHumidity(day.humidity);
+      let windSpeed = getWindSpeed(day.wind_speed);
 
       spanDateEl.text(date);
       spanHiEl.html(hi);
@@ -173,6 +206,21 @@ $(document).ready(function () {
 
       forecastEl.append(divEl);
     });
+    forecastEl.css("display", "flex");
+  }
+
+  function getUviHtml(uvi) {
+    className = "";
+    if (uvi < 3) {
+      className = "good";
+    } else if (uvi > 3 && uvi < 6) {
+      className = "moderate";
+    } else if (uvi > 6 && uvi < 8) {
+      className = "warning";
+    } else {
+      className = "danger";
+    }
+    return `<span class='${className}'>UV Index: ${uvi}</span>`;
   }
 
   function unixToDate(unix, format) {
@@ -191,6 +239,13 @@ $(document).ready(function () {
     return img;
   }
 
+  function getHumidity(humidity) {
+    return `Humidity: ${humidity}%`;
+  }
+
+  function getWindSpeed(windSpeed) {
+    return `Wind: ${windSpeed.toFixed(1)} MPH`;
+  }
   function getWindDirection(wind) {
     let d = wind.deg;
 
@@ -212,10 +267,9 @@ $(document).ready(function () {
     console.log($("#citySearch").val());
 
     // set city
-    city = e.target[0].value;
+    let formCityValue = e.target[0].value;
 
-    getLongLat(city);
-    //fetchData();
+    getLongLat(formCityValue);
   });
 
   function makeAccordion() {
